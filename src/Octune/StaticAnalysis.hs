ac0c425daa9225a93675a431478d2ecaa922e2da
Gary Feng <gary.feng@uwaterloo.ca>
2021-06-19 12:18:36 -0400

    Complete checking BeatsAssertions

--- src/Octune/StaticAnalysis.hs
+++ src/Octune/StaticAnalysis.hs
@@ -4,9 +4,15 @@ import           Data.Foldable
 
 import           Data.Text                      (Text)
 
+import           Octune.StaticAnalysis.BarBeats
 import           Octune.StaticAnalysis.VarUsage
 import           Octune.Types
 
 staticAnalysis :: Env (AST Ann) -> AST Ann -> Either Text ()
-staticAnalysis env ast =
-    traverse_ ($ env) [checkVarsDeclared, checkNoVarCycles]
+staticAnalysis env ast = traverse_ ($ env) checks
+  where
+    checks =
+        [ checkVarsDeclared
+        , checkNoVarCycles
+        , checkBeatsAssertions
+        ]
