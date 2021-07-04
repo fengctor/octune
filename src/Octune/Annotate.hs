de24b7c6a524ab686e34e544cae0f646bcd07f7e
Gary Feng <gary.feng@uwaterloo.ca>
2021-07-04 18:30:05 -0400

    Add prisms for the AST and use them for annotating beat lengths

--- src/Octune/Annotate.hs
+++ src/Octune/Annotate.hs
@@ -17,39 +17,36 @@ annotateBeatLengths env = cache
     memoAnnotate expr          = go expr
 
     go :: AST Ann -> AST Ann
-    go (Song ann bpm expr) =
-        Song newAnnotation bpm annotatedExpr
+    go s@(Song _ _ expr) =
+        s & _Song . _3
+          .~ annotatedExpr
+          & annotation . beatLength
+          .~ annotatedExpr ^. annotation . beatLength
       where
         annotatedExpr = memoAnnotate expr
-        thisBeatLength = annotatedExpr ^. annotation . beatLength
-        newAnnotation = ann & beatLength .~ thisBeatLength
-    go (Var ann vName) =
-        Var newAnnotation vName
-      where
-        annotatedExpansion = memoAnnotate (env Map.! vName)
-        thisBeatLength = annotatedExpansion ^. annotation . beatLength
-        newAnnotation = ann & beatLength .~ thisBeatLength
-    go (LineNote ann note@(Note _ beats _)) =
-        LineNote newAnnotation note
-      where
-        newAnnotation = ann & beatLength ?~ beats
-    go (LineApp ann lFun args) =
-        LineApp newAnnotation lFun annotatedArgs
+    go v@(Var _ vName) =
+        v & annotation . beatLength
+          .~ memoAnnotate (env Map.! vName) ^. annotation . beatLength
+    go n@(LineNote _ (Note _ beats _)) =
+        n & annotation . beatLength
+          ?~ beats
+    go a@(LineApp _ lFun args) =
+        a & _LineApp . _3
+          .~ annotatedArgs
+          & annotation . beatLength
+          ?~ foldlOf'
+                 (traversed . annotation . beatLength . _Just)
+                 (\acc cur -> combineFun acc (beatMod cur))
+                 0
+                 annotatedArgs
       where
         annotatedArgs = fmap memoAnnotate args
-        (combineFun, beatFun) =
+        (combineFun, beatMod) =
             case lFun of
                 Seq      -> ((+), id)
                 Merge    -> (max, id)
                 Repeat n -> ((+), (* toRational n))
                 Volume _ -> ((+), id)
-        thisBeatLength =
-            foldlOf'
-                (traversed . annotation . beatLength . _Just)
-                (\a c -> combineFun a (beatFun c))
-                0
-                annotatedArgs
-        newAnnotation = ann & beatLength ?~ thisBeatLength
     -- Beats assertions do not take up time
     go b@BeatsAssertion{} = b
     go _ = error "Should not have File or Decl from parsing"
--- src/Octune/Types/AST.hs
+++ src/Octune/Types/AST.hs
@@ -55,3 +55,55 @@ annotation handler (LineApp ann lFun args) =
     LineApp <$> handler ann <^> lFun <^> args
 annotation handler (BeatsAssertion ann mBeats) =
     BeatsAssertion <$> handler ann <^> mBeats
+
+
+-- Prisms for `AST a`
+
+_File :: Prism' (AST a) (a, [Text], [AST a])
+_File = prism' embed match
+  where
+    embed (ann, m, ds) = File ann m ds
+    match (File ann m ds) = Just (ann, m, ds)
+    match _               = Nothing
+
+_Decl :: Prism' (AST a) (a, Text, AST a)
+_Decl = prism' embed match
+  where
+    embed (ann, v, expr) = Decl ann v expr
+    match (Decl ann v expr) = Just (ann, v, expr)
+    match _                 = Nothing
+
+_Song :: Prism' (AST a) (a, Int, AST a)
+_Song = prism' embed match
+  where
+    embed (ann, bpm, expr) = Song ann bpm expr
+    match (Song ann bpm expr) = Just (ann, bpm, expr)
+    match _                   = Nothing
+
+_Var :: Prism' (AST a) (a, QualifiedName)
+_Var = prism' embed match
+  where
+    embed (ann, v) = Var ann v
+    match (Var ann v) = Just (ann, v)
+    match _           = Nothing
+
+_LineNote :: Prism' (AST a) (a, Note)
+_LineNote = prism' embed match
+  where
+    embed (ann, note) = LineNote ann note
+    match (LineNote ann note) = Just (ann, note)
+    match _                   = Nothing
+
+_LineApp :: Prism' (AST a) (a, LineFun, [AST a])
+_LineApp = prism' embed match
+  where
+    embed (ann, lFun, args) = LineApp ann lFun args
+    match (LineApp ann lFun args) = Just (ann, lFun, args)
+    match _                       = Nothing
+
+_BeatsAssertion :: Prism' (AST a) (a, Maybe Beats)
+_BeatsAssertion = prism' embed match
+  where
+    embed (ann, mBeats) = BeatsAssertion ann mBeats
+    match (BeatsAssertion ann mBeats) = Just (ann, mBeats)
+    match _                           = Nothing
